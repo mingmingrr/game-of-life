b50d8b70a17c0abacbe9bff6a4b2a5cb7d4429f7
mingmingrr <mingmingrr@gmail.com>
2024-07-30 12:27:02 -0400

    life: complete

--- life.cpp
+++ life.cpp
@@ -5,9 +5,13 @@
 #include <ostream>
 #include <chrono>
 #include <cstdlib>
+#include <thread>
 #include <string>
+#include <iostream>
 #include <sul/dynamic_bitset.hpp>
 #include <ncursesw/curses.h>
+
+using namespace std::chrono_literals;
 
 struct tracetime {};
 struct tracetime now;
@@ -58,19 +62,33 @@ decltype(auto) trace(Xs&& ...xs) {
 	return (identity(std::forward<Xs>(xs)), ...);
 }
 
+struct CursesWin {
+	CursesWin() {
+		initscr();
+		cbreak();
+		noecho();
+		curs_set(false);
+		nodelay(stdscr, true);
+		keypad(stdscr, true);
+		mousemask(ALL_MOUSE_EVENTS, NULL);
+	}
+	~CursesWin() {
+		keypad(stdscr, false);
+		echo();
+		nocbreak();
+		endwin();
+	}
+};
 
 int main() {
 	trace(now, "========================================");
+	std::locale::global(std::locale(""));
 
 	srand(time(NULL));
-	WINDOW* win = initscr();
-	cbreak();
-	curs_set(0);
-	mousemask(ALL_MOUSE_EVENTS, NULL);
-	// nodelay(win, true);
+	CursesWin _cwin;
 
-	static wchar_t blocks[] = L" ▀▄█";
-	const int lines = getmaxy(win), cols = getmaxx(win);
+	const wchar_t blocks[] = L" ▀▄█";
+	const int lines = getmaxy(stdscr), cols = getmaxx(stdscr);
 
 	const int inner_rows = 2 * lines, inner_cols = cols;
 	const int outer_rows = inner_rows + 2, outer_cols = inner_cols + 2;
@@ -97,82 +115,57 @@ int main() {
 		= outer_top | outer_bot | outer_left | outer_right;
 	sul::dynamic_bitset<> inner_mask = ~outer_mask;
 
+	sul::dynamic_bitset<> neigh_mask(outer_squares);
+	for(int i = 0; i < 3; ++i)
+		neigh_mask.set(outer_cols * i, 3, true);
+
 	sul::dynamic_bitset<> grid(outer_squares);
 	for(int i = 0; i < outer_squares; ++i)
 		grid.set(i, rand() % 4 == 0);
 
-	trace(now, grid);
-
 	clear();
-	trace(now, lines, inner_cols);
-	for(int r = 0; r < lines; ++r) {
-		std::wstring line(inner_cols, '-');
-		for(int c = 0; c < inner_cols; ++c)
-			line[c] = blocks[grid[(2 * r + 1) * outer_cols + c + 1]
-				+ (grid[(2 * r + 2) * outer_cols + c + 1] << 1)];
-		trace(now, "line:");
-		trace('>', line, '<');
-		trace("lineend");
-		mvins_nwstr(r, 0, line.data(), inner_cols);
-	}
 
-	getch();
+	while(getch() == -1) {
+		for(int r = 0; r < lines; ++r) {
+			std::wstring line(inner_cols, '-');
+			for(int c = 0; c < inner_cols; ++c)
+				line[c] = (int)blocks[grid[(2 * r + 1) * outer_cols + c + 1]
+					+ (grid[(2 * r + 2) * outer_cols + c + 1] << 1)];
+			mvins_nwstr(r, 0, line.data(), inner_cols);
+		}
+
+		std::this_thread::sleep_for(100ms);
+
+		grid &= inner_mask;
+		grid |= ((grid >> inner_cols) & outer_left)
+			| ((grid << inner_cols) & outer_right);
+		grid |= ((grid >> inner_squares) & outer_top)
+			| ((grid << inner_squares) & outer_bot);
+
+		sul::dynamic_bitset<> checks = grid | (grid << 1) | (grid >> 1);
+		checks |= (grid << outer_cols) | (grid >> outer_cols);
+		checks &= inner_mask;
+
+		sul::dynamic_bitset<> threes(outer_squares);
+		sul::dynamic_bitset<> fours(outer_squares);
+		checks.iterate_bits_on([&](int bit) {
+			int count = (grid & (neigh_mask << (bit - 1 - outer_cols))).count();
+			switch(count) {
+				case 3: threes.set(bit); break;
+				case 4: fours.set(bit); break;
+			}
+		});
+
+		grid = (grid & (threes | fours)) | (~grid & threes);
 
 /*
-	= random.randint(0, grid_mask)
-	grid &= random.randint(0, grid_mask)
-	stdscr.clear()
-	for r in range(lines):
-		l = grid >> ((2 * r + 1) * outer_cols + 1)
-		stdscr.insstr(r, 0, ''.join(
-			blocks[((l >> c) & 1) | ((l >> (c + outer_cols - 1)) & 2)]
-			for c in range(cols)))
-
-	generate = 0
-
-	while stdscr.getch() == -1:
-		time.sleep(0.05)
-
-		grid &= inner_mask
-		grid |= ((grid >> inner_cols) & outer_left) | ((grid << inner_cols) & outer_right)
-		grid |= ((grid >> inner_squares) & outer_top) | ((grid << inner_squares) & outer_bot)
-
-		checks = grid | (grid << 1) | (grid >> 1)
-		checks |= (grid << outer_cols) | (grid >> outer_cols)
-		checks &= inner_mask
-
-		life = grid
-
-		while checks:
-			bit = checks & -checks
-			checks &= ~bit
-
-			neigh = bit | (bit << 1) | (bit >> 1)
-			neigh |= (neigh << outer_cols) | (neigh >> outer_cols)
-			neigh = (neigh & grid).bit_count()
-
-			if (grid & bit):
-				if not (3 <= neigh <= 4):
-					life &= ~bit
-			elif neigh == 3:
-					life |= bit
-
-		grid = life
-
-		generate += 1
-		if generate == 10:
-			generate, times = 0, round(outer_rows * outer_cols
+		generation += 1
+		if generation == 10:
+			generation, times = 0, round(outer_rows * outer_cols
 				/ max(1, grid.bit_count()) / 10)
 			for _ in range(times):
 				grid |= 1 << random.randint(0, outer_rows * outer_cols - 1)
-
-		for r in range(lines):
-			l = grid >> ((2 * r + 1) * outer_cols + 1)
-			stdscr.insstr(r, 0, ''.join(
-				blocks[((l >> c) & 1) | ((l >> (c + outer_cols - 1)) & 2)]
-				for c in range(cols)))
-
-curses.wrapper(main)
 */
+	}
 }
 
